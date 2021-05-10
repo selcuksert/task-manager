cd271cf64653ab02d63a2e751e2ed2995f45c037
selcuksert <selcuk.sert@gmail.com>
2021-05-11 01:19:48 +0300

    Added OpenID Connect and OAuth2 support with utilizing Keycloak as IdP

---
+++ services/task-reader/src/main/java/com/corp/concepts/taskmanager/services/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.corp.concepts.taskmanager.services.configuration;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import com.corp.concepts.taskmanager.services.configuration.jwt.JwtAuthConverter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private Converter<Jwt, AbstractAuthenticationToken> converter;
+	
+	public SecurityConfig(JwtAuthConverter converter) {
+		this.converter = converter;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().disable().authorizeRequests()
+		.antMatchers("/api/task/reader").hasAnyRole("manager", "user")
+		.anyRequest().denyAll().and()
+		.oauth2ResourceServer().jwt()
+		.jwtAuthenticationConverter(converter);
+	}
+
+}
---
+++ services/task-reader/src/main/java/com/corp/concepts/taskmanager/services/configuration/jwt/JwtAuthConverter.java
@@ -0,0 +1,51 @@
+package com.corp.concepts.taskmanager.services.configuration.jwt;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+	private final JwtGrantedAuthoritiesConverter defaultGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+	@Value("${custom.security.provider.jwt.claim.key}")
+	private String resourceKey;
+
+	@Value("${custom.security.provider.jwt.role.key}")
+	private String roleKey;
+
+	private Collection<? extends GrantedAuthority> extractResourceRoles(final Jwt jwt) {
+		Map<String, Collection<String>> resourceAccess = jwt.getClaim(resourceKey);
+		Collection<String> resourceRoles;
+		
+		if (resourceAccess != null && (resourceRoles = (Collection<String>) resourceAccess.get(roleKey)) != null) {
+			return resourceRoles.stream().map(x -> new SimpleGrantedAuthority("ROLE_" + x)).collect(Collectors.toSet());
+		}
+		
+		return Collections.emptySet();
+	}
+
+	@Override
+	public AbstractAuthenticationToken convert(Jwt source) {
+		Collection<GrantedAuthority> authorities = Stream
+				.concat(defaultGrantedAuthoritiesConverter.convert(source).stream(),
+						extractResourceRoles(source).stream())
+				.collect(Collectors.toSet());
+
+		return new JwtAuthenticationToken(source, authorities);
+	}
+
+}

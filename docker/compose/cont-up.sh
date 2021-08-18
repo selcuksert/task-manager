5e3f7064493150429422081f3debfabec9d1e525
selcuksert <selcuk.sert@gmail.com>
2021-08-18 10:24:39 +0300

    Various enhancements

--- docker/compose/cont-up.sh
+++ docker/compose/cont-up.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker compose -f $(dirname $0)/docker-compose.base.yml -f $(dirname $0)/docker-compose.containerized.yml up --build --remove-orphans
+docker compose -f $(dirname $0)/docker-compose.base.yml -f $(dirname $0)/docker-compose.containerized.yml up -d --build --remove-orphans
